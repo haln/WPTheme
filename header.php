b4fb23b75912d2c085a122729f9fd40f240e8106
evelyndai <daiyi68@live.cn>
2016-04-02 15:00:40 -0700

    header and footer

---
+++ header.php
@@ -0,0 +1,15 @@
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--&#91;if lt IE 9&#93;>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<!&#91;endif&#93;-->
+	<?php wp_head(); ?>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+</head>
+<header class="header">
+        <img alt="League of Legend" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" />
+</header>
